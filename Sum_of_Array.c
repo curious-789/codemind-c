e1af0919f932a3cd99fee4f5134198eda6f06155
charan <84625492+curious-789@users.noreply.github.com>
2022-10-27 10:23:47 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n,o=0,sum=0;     cin>>n;     int arr[n];     for(int i=0;i<n;i++){         cin>>arr[i];     }     //sum=0     for(int i=0;i<n;i++){     sum=sum+arr[i];     }     cout<<sum;     return 0; }

---
+++ Sum_of_Array.c
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,o=0,sum=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    //sum=0
+    for(int i=0;i<n;i++){
+    sum=sum+arr[i];
+    }
+    cout<<sum;
+    return 0;
+}
